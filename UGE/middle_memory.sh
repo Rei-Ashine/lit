23182f24721316576d7047e2a6ddf73233be93cd
Rei-Ashine <ashine.rei.74z@st.kyoto-u.ac.jp>
2021-05-13 15:43:57 +0900

    Cleanup scripts

--- UGE/middle_memory.sh
+++ UGE/middle_memory.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 #$ -S /usr/bin/bash
 #$ -cwd
-#$ -N Jnote
-#$ -o dev/null
-#$ -e dev/null
+#$ -N Middle
+#$ -o /dev/null
+#$ -e /dev/null
 #$ -l s_vmem=64G
 #$ -l mem_req=64G
-# qsub jnote.sh
+# qsub middle_memory.sh
 
 echo JOB_ID: $JOB_ID
 echo JOB_NAME: $JOB_NAME
 
-bash ../jnote.sh
+bash ../jupyter-notebook.sh
