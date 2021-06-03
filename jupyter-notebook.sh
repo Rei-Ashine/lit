b25a671ccf03a824b81578bc78e03ac6a4e9315b
Rei-Ashine <ashine.rei.74z@st.kyoto-u.ac.jp>
2021-06-03 21:26:20 +0900

    Fix script's format

--- jupyter-notebook.sh
+++ jupyter-notebook.sh
@@ -2,9 +2,10 @@
 #$ -S /usr/bin/bash
 # bash jnote.sh
 
+echo
 date
-echo "Executing $(basename $0)"
-echo ==================================================
+echo "========== Executing $(basename $0) =========="
 cd $(dirname $0)
+
 
 jupyter notebook
